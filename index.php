4390f3cf2458cc1891250cae050f8dfaadf67128
pietrodn <powerpdn@gmail.com>
2015-01-01 09:20:59 +0100

    Updated email contact

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 	echo "  <link>https://" . WIKI_HOST . "</link>\n";
 	echo "  <description>Tutte le notizie in tempo reale!</description>\n";
 	echo "  <language>it</language>\n";
-	echo "  <webMaster>pietrodn@toolserver.org (Pietrodn)</webMaster>\n";
+	echo "  <webMaster>itwikinews-rss.help@tools.wmflabs.org (Pietrodn)</webMaster>\n";
 	echo "  <atom:link href=\"https://tools.wmflabs.org/itwikinews-rss/\" rel=\"self\" type=\"application/rss+xml\" />\n";
 	echo "  <copyright>CC-BY-SA-3.0</copyright>\n";
 	echo "  <generator>https://github.com/pietrodn/itwikinews-rss/</generator>\n";
